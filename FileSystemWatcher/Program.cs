256e237036b2c9c3b535f7608efff51643e6906e
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2021-11-28 09:09:22 -0300

    FileSystemWatcher

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,27 @@
+﻿var path = @"c:\temp\globo";
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Renamed += OnRenamed;
+fsw.Deleted += OnDeleted;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine($"Monitorando eventos na pasta {path}");
+Console.WriteLine("Pressione [enter] para finalizar....");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi excluído o arquivo {e.Name}");
+}
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"O arquivo {e.OldName} foi renomeado para {e.Name}");
+}
