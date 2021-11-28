e47c76dde93110162515e4bb1773952d247f192e
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2021-11-28 19:23:04 -0300

    StringReader

---
+++ StringReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+var sb = new StringBuilder();
+sb.AppendLine("Caracteres na primeira linha para ler");
+sb.AppendLine("e a segunda linha");
+sb.AppendLine("e o fim");
+
+using var sr = new StringReader(sb.ToString());
+// var buffer = new char[10];
+// var tamanho = 0;
+
+do
+{
+    Console.WriteLine(sr.ReadLine());
+}while(sr.Peek() >= 0);
+// do
+// {
+//     buffer = new char[10];
+//     tamanho = sr.Read(buffer);
+//     Console.Write(string.Join("",buffer));
+
+// }while(tamanho >= buffer.Length);
+
+Console.WriteLine("Digite [enter] para finalizar");
+Console.ReadKey();
